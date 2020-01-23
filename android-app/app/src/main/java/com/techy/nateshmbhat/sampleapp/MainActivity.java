17e7eeac9fa6bc7df43dc009ef58e7a0e8eb77b9
nateshmbhat <nateshmbhat1@gmail.com>
2020-01-23 10:36:42 +0530

    update

--- android-app/app/src/main/java/com/techy/nateshmbhat/sampleapp/MainActivity.java
+++ android-app/app/src/main/java/com/techy/nateshmbhat/sampleapp/MainActivity.java
@@ -3,6 +3,10 @@ package com.techy.nateshmbhat.sampleapp;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,17 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Button myButton = findViewById(R.id.button) ;
+
+        myButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                startActivity(
+                        FlutterActivity.createDefaultIntent(MainActivity.this)
+                );
+            }
+        });
     }
 }
---
+++ flutter_module_android_studio/README.md
@@ -0,0 +1,8 @@
+# flutter_module_android_studio
+
+A new Flutter module.
+
+## Getting Started
+
+For help getting started with Flutter, view our online
+[documentation](https://flutter.dev/).
